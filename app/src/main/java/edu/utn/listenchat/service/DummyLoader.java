6f9d6be747deaede79f77197db445cc2783c2ba9
Fabian Cardaci <fabian.cardaci@pmitn010885.local>
2017-10-27 16:00:46 -0300

    change dummy loader

--- app/src/main/java/edu/utn/listenchat/service/DummyLoader.java
+++ app/src/main/java/edu/utn/listenchat/service/DummyLoader.java
@@ -20,25 +20,21 @@ public class DummyLoader {
 
         List<Message> messages = Lists.newArrayList();
 
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 1", toDate("2017-09-05T00:11:22"), "I"));
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 2", toDate("2017-09-05T00:11:22"), "I"));
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 3", toDate("2017-09-05T00:11:22"), "I"));
+        messages.add(Message.create("Fabián Cardaci", "Hola 1", toDate("2017-09-05T00:11:22"), "I"));
+        messages.add(Message.create("Fabián Cardaci", "Hola 2", toDate("2017-09-05T00:11:22"), "I"));
+        messages.add(Message.create("Fabián Cardaci", "Hola 3", toDate("2017-09-05T00:11:22"), "I"));
 
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 11", toDate("2017-09-06T00:11:22"), "I"));
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 22", toDate("2017-09-06T00:11:22"), "I"));
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 33", toDate("2017-09-06T00:11:22"), "I"));
+        messages.add(Message.create("Fabián Cardaci", "Hola 11", toDate("2017-09-06T00:11:22"), "I"));
+        messages.add(Message.create("Fabián Cardaci", "Hola 12", toDate("2017-09-06T00:11:22"), "I"));
+        messages.add(Message.create("Fabián Cardaci", "Hola 13", toDate("2017-09-06T00:11:22"), "I"));
 
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 111", toDate("2017-09-07T00:11:22"), "I"));
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 222", toDate("2017-09-07T00:11:22"), "I"));
-        messages.add(Message.create("Cacho Garay", "Hola soy cacho 333", toDate("2017-09-07T00:11:22"), "I"));
+        messages.add(Message.create("Fabi De La Cruz", "Chau 1", toDate("2017-09-05T00:11:22"), "I"));
+        messages.add(Message.create("Fabi De La Cruz", "Chau 2", toDate("2017-09-05T00:11:22"), "I"));
+        messages.add(Message.create("Fabi De La Cruz", "Chau 3", toDate("2017-09-05T00:11:22"), "I"));
 
-        messages.add(Message.create("Sergio Gonal", "Hola soy sergio 1", toDate("2017-09-08T00:11:22"), "I"));
-        messages.add(Message.create("Sergio Gonal", "Hola soy sergio 2", toDate("2017-09-08T00:11:22"), "I"));
-        messages.add(Message.create("Sergio Gonal", "Hola soy sergio 3", toDate("2017-09-08T00:11:22"), "I"));
-
-        messages.add(Message.create("Sergio Gonal", "Hola soy sergio 11", toDate("2017-09-09T00:11:22"), "I"));
-        messages.add(Message.create("Sergio Gonal", "Hola soy sergio 22", toDate("2017-09-09T00:11:22"), "I"));
-        messages.add(Message.create("Sergio Gonal", "Hola soy sergio 33", toDate("2017-09-09T00:11:22"), "I"));
+        messages.add(Message.create("Fabi De La Cruz", "Chau 11", toDate("2017-09-06T00:11:22"), "I"));
+        messages.add(Message.create("Fabi De La Cruz", "Chau 12", toDate("2017-09-06T00:11:22"), "I"));
+        messages.add(Message.create("Fabi De La Cruz", "Chau 13", toDate("2017-09-06T00:11:22"), "I"));
 
         persistenceService.insert(messages);
     }
