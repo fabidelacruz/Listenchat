88bb050cfe563dd9726ba98b09d45d6947e92e95
Fabian Cardaci <fabian.cardaci@labazul-pc0049.labsistemas.dom>
2017-11-04 16:52:25 -0300

    comandos

--- app/src/main/java/edu/utn/listenchat/handler/common/CommandsHandler.java
+++ app/src/main/java/edu/utn/listenchat/handler/common/CommandsHandler.java
@@ -16,7 +16,7 @@ public class CommandsHandler {
     private TextToSpeechService textToSpeechService;
 
     public void handleCommands() {
-        this.textToSpeechService.speak("Comandos");
+        this.textToSpeechService.speak("Comandos disponobles");
         this.textToSpeechService.speak(NOVELTIES.getText());
         this.textToSpeechService.speak(NEW_MESSAGES.getText());
         this.textToSpeechService.speak(CONVERSATION_WITH.getText());
